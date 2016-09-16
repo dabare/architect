d6a875961a0e32f9a2cab793d35e3286ddea6c1e
dabare <dabaremadhava@gmail.com>
2016-09-16 18:58:52 +0530

    set database in cloud

--- db/user.php
+++ db/user.php
@@ -1,7 +1,7 @@
 <?php 
-	$servername="localhost";
-	$username = "root";
-	$password ="aelo";
-	$dbname = "architect";
+	$servername="sql6.freemysqlhosting.net";
+	$username = "sql6136050";
+	$password ="WLqA8FIy76";
+	$dbname = "sql6136050";
 
  ?>
