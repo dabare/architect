9f01657c2ace66c90c5ae802c8b828bdfdf525ba
CHOBODI <CHOBODI>
2017-01-13 10:10:03 +0530

    db

--- db/user.php
+++ db/user.php
@@ -1,9 +1,6 @@
 <?php 
-	$servername="localhost";
-	$username = "root";
-	$password ="";
+	$servername="us-cdbr-azure-southcentral-f.cloudapp.net";
+	$username = "b644a924bb3dc3";
+	$password ="3abfdcf5";
 	$dbname = "architect";
-
  ?>
- 
- 
